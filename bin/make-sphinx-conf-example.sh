2e3a648366d1155ff0aea8f919c5002cbb5f4d36
Gilmar Pupo <g@g1mr.com>
2014-08-11 15:09:21 -0300

    merging index

--- bin/make-sphinx-conf-example.sh
+++ bin/make-sphinx-conf-example.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# file: https://raw.githubusercontent.com/gpupo/pipe2/master/bin/make-sphinx-conf-example.sh
+##
 
 wget https://raw.githubusercontent.com/gpupo/pipe2/master/data/sphinx.sample.conf -O /etc/sphinx/sphinx.conf;
 
@@ -6,10 +8,8 @@ mkdir -p /tmp/data/;
 wget https://raw.githubusercontent.com/gpupo/pipe2/master/data/acme.googleshopping.xml -O /tmp/data/acme.googleshopping.xml;
 wget https://raw.githubusercontent.com/gpupo/pipe2/master/data/foo.googleshopping.xml -O /tmp/data/foo.googleshopping.xml;
 
-service searchd stop;
-/usr/bin/indexer --rotate --all;
-indexer --merge main acmeIndex
-indexer --merge main fooIndex
 service searchd start;
 
+indexer --merge main acmeIndex --rotate;
+indexer --merge main fooIndex --rotate;
 
