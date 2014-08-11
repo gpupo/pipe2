1c407bc94dfaf7cae409097577deee219d01cbf9
Gilmar Pupo <g@g1mr.com>
2014-08-11 14:47:19 -0300

    fix blank document

--- src/Gpupo/Pipe2/Converter/BlankConverter.php
+++ src/Gpupo/Pipe2/Converter/BlankConverter.php
@@ -6,9 +6,36 @@ use Gpupo\CommonSchema\Sphinx\GoogleSchema;
 
 class BlankConverter extends GoogleConverter
 {
-    public function execute()
+    protected function parser()
     {
-        return $this;
+        return array(
+            array(
+                'sku' => '01',
+                'title' => 'blank',
+                'description' => 'blank',
+                'category' => '',
+                'type' => 'document',
+                'brand' => '',
+                'size' => '',
+                'color' => '',
+                'gender' => '',
+                'link' => '',
+                'id' => '01',
+                'price' => '',
+                'sale_price' => '',
+                'sale_price_effective_date' => '',
+                'mpn' => '',
+                'image_link' => '',
+                'condition' => '',
+                'availability' => '',
+                'age_group' => '',
+                'shipping_weight' => '',
+                'online_only' => '',
+                'installment_months' => '',
+                'installment_amount' => '',
+                'review_count' => '',
+                'review_average' => '',
+            )
+        );
     }
-
 }
