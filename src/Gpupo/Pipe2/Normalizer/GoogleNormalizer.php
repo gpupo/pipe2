3c4ce6021802627e351b1601ebb36ebec93fbdd0
Gilmar Pupo <g@g1mr.com>
2014-08-14 09:58:48 -0300

    Extended convert and Normalizers

---
+++ src/Gpupo/Pipe2/Normalizer/GoogleNormalizer.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Gpupo\Pipe2\Normalizer;
+
+class GoogleNormalizer extends  NormalizerAbstract implements NormalizerInterface
+{
+
+}
---
+++ src/Gpupo/Pipe2/Normalizer/NormalizerDefault.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Gpupo\Pipe2\Normalizer;
+
+final class NormalizerDefault extends NormalizerAbstract implements NormalizerInterface
+{
+}
