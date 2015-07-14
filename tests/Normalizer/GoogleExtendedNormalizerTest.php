66ab6fe2877f333ae1a24d8d648196e9c4be284b
Gilmar Pupo <g@g1mr.com>
2015-07-14 09:54:33 -0300

    adaptação do phpunit.xml.dist

--- tests/Normalizer/GoogleExtendedNormalizerTest.php
+++ tests/Normalizer/GoogleExtendedNormalizerTest.php
@@ -3,6 +3,7 @@
 namespace Gpupo\Tests\Pipe2\Normalizer;
 
 use Gpupo\Pipe2\Normalizer\GoogleExtendedNormalizer;
+use Gpupo\Tests\Pipe2\TestCaseAbstract;
 
 class GoogleExtendedNormalizerTest extends TestCaseAbstract
 {
