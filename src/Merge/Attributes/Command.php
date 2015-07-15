be6560358ce4a58a3735025c6e65492c811df393
Gilmar Pupo <g@g1mr.com>
2015-07-15 09:25:53 -0300

    merge-attributes <firstDocument> <secondDocument> <output>
     Arguments:
    firstDocument         First Document Xml file path
    secondDocument        Second Document Xml file path
    output                Output Document Xml file path

--- src/Merge/Attributes/Command.php
+++ src/Merge/Attributes/Command.php
@@ -12,7 +12,10 @@
 namespace Gpupo\Pipe2\Merge\Attributes;
 
 use Symfony\Component\Console\Command\Command as Core;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
 
 class Command extends Core
 {
@@ -21,14 +24,21 @@ class Command extends Core
         parent::configure();
         $this
             ->setName('merge-attributes')
-            ->setDescription('Merge XML Documents with Similar Structure Where Second Document Contains Attributes');
-    }
-
-    protected function getParameters(InputInterface $input)
-    {
-        $parameters = parent::getParameters($input);
-        $parameters['input'] = '/dev/null';
-
-        return $parameters;
+            ->setDescription('Merge XML Documents with Similar Structure Where Second Document Contains Attributes')
+            ->addArgument(
+                'firstDocument',
+                InputArgument::REQUIRED,
+                'First Document Xml file path'
+            )
+            ->addArgument(
+                'secondDocument',
+                InputArgument::REQUIRED,
+                'Second Document Xml file path'
+            )
+            ->addArgument(
+                'output',
+                InputArgument::REQUIRED,
+                'Output Document Xml file path'
+            );
     }
 }
