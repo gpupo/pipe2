266c34b671403611a4c2c3097d9f5d74b225c8de
Gilmar Pupo <g@g1mr.com>
2015-07-15 17:17:40 -0300

    Merge Output OK

--- src/Merge/Attributes/Combiner.php
+++ src/Merge/Attributes/Combiner.php
@@ -21,22 +21,39 @@ class Combiner
     use ParserTrait;
     use DocumentContainerTrait;
 
-    protected $idKey = 'id';
+    protected $idField;
+
+    protected function getIdField()
+    {
+        return $this->idField;
+    }
 
     public function __construct(Array $parameters)
     {
-        $this->setDocument(new Document());
-
-        $this->idKey = 'g:id';
-
+        $this->setDocument(new Document(), $parameters['formatOutput']);
+        $this->idField = $parameters['idField'];
         $firstDocument = $this->factoryCollectionFromFilePath($parameters['firstDocument']);
         $secondDocument = $this->factoryCollectionFromFilePath($parameters['secondDocument']);
-        $this->combine($firstDocument, $secondDocument);
+        $this->appendItens($this->combine($firstDocument, $secondDocument));
     }
 
     protected function factoryCollectionFromFilePath($filePath)
     {
         return new Collection($this->parserProcess($this->parserFromFile($filePath)));
+    }
+
+    protected function hasKey($item)
+    {
+        if ($item instanceof CollectionInterface && $item->containsKey($this->getIdField())) {
+            return true;
+        }
+
+        if (array_key_exists($this->getIdField(), $item)) {
+            return true;
+        }
+        error_log('Warning:Item Field ['.$this->getIdField().'] not found. Try setup --idField options.');
+
+        return false;
     }
 
     protected function parserProcess(Array $raw)
@@ -48,7 +65,10 @@ class Combiner
                 continue;
             }
             $item = $this->parserItems($data);
-            $list[$item[$this->idKey]] = new Collection($item);
+
+            if ($this->hasKey($item)) {
+                $list[$item[$this->getIdField()]] = new Collection($item);
+            }
         }
 
         return $list;
@@ -57,7 +77,11 @@ class Combiner
     protected function combine(CollectionInterface $first, CollectionInterface $second)
     {
         foreach($first as $item) {
-            $key =  $item->get($this->idKey);
+            if (!$this->hasKey($item)) {
+                continue;
+            }
+            $key =  $item->get($this->idField);
+
             if($second->containsKey($key)) {
                 foreach($second->get($key) as $field => $value)
                 {
@@ -70,4 +94,23 @@ class Combiner
         return $first;
     }
 
+    protected function appendItens($list)
+    {
+        foreach ($list as $item) {
+            $itemElement = $this->document->createElement('item');
+            $id = $item->get($this->getIdField());
+            $itemElement->setAttribute('id', $id);
+
+            foreach ($item as $key => $value) {
+                $tag = $this->document->createElement($key);
+                $tag->appendChild(
+                    $this->document->createTextNode($value)
+                );
+
+                $itemElement->appendChild($tag);
+            }
+
+            $this->document->docset->appendChild($itemElement);
+        }
+    }
 }
