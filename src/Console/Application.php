616f8b3b53ecf6313b3282412497dc67cfdf127a
Gilmar Pupo <g@g1mr.com>
2015-08-19 09:19:42 -0300

    Common Console

---
+++ src/Console/Application.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of gpupo/pipe2
+ *
+ * (c) Gilmar Pupo <g@g1mr.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * For more information, see
+ * <http://www.g1mr.com/pipe2/>.
+ */
+
+namespace Gpupo\Pipe2\Console;
+
+use Gpupo\Common\Console\AbstractApplication;
+
+class Application extends AbstractApplication
+{
+    
+}
